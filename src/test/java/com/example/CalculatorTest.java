9b1f85a8cd17084e3a99a0e80dc74f99e3bb4d5c
Akshay Dhengre <akshaydhengre430@gmail.com>
2025-12-31 18:34:50 +0530

    updates

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -11,7 +11,7 @@ public class CalculatorTest {
 
     @Test
     public void testAddPositive() {
-        Assert.assertEquals(add(2, 3), 5);
+        Assert.assertEquals(add(3, 2), 5);
     }
 
     @Test

fe7cc8edf531e1b9e6a5a0fe35e36eadb9ecce26
Akshay Dhengre <akshaydhengre430@gmail.com>
2025-12-31 18:01:00 +0530

    updates

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Test
+    public void testAddPositive() {
+        Assert.assertEquals(add(2, 3), 5);
+    }
+
+    @Test
+    public void testAddNegative() {
+        Assert.assertEquals(add(-2, -3), -5);
+    }
+}

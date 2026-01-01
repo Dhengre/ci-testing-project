8a050d132dd02eeacf515ee30d32d0737448e240
Akshay Dhengre <akshaydhengre430@gmail.com>
2026-01-01 13:36:47 +0530

    latest change

---
+++ src/test/java/com/example/CalculatorFailTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class CalculatorFailTest {
+
+	Calculator calc = new Calculator();  
+    @Test
+    public void testMultiplyFail() {
+        int expected = 50; // ❌ wrong expectation
+        int actual = calc.multiply(5, 5);
+
+        System.out.println("FAIL TEST: Multiplication");
+        System.out.println("Expected = " + expected);
+        System.out.println("Actual   = " + actual);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testAddFail() {
+        int expected = 20; // ❌ wrong expectation
+        int actual = calc.add(10, 5);
+
+        System.out.println("FAIL TEST: Addition");
+        System.out.println("Expected = " + expected);
+        System.out.println("Actual   = " + actual);
+
+        Assert.assertEquals(actual, expected);
+    }
+}
---
+++ src/test/java/com/example/CalculatorPassTest.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorPassTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        int expected = 8;
+        int actual = calc.add(5, 3);
+
+        System.out.println("PASS TEST: Addition");
+        System.out.println("Expected = " + expected);
+        System.out.println("Actual   = " + actual);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testSubtract() {
+        int expected = 4;
+        int actual = calc.subtract(7, 3);
+
+        System.out.println("PASS TEST: Subtraction");
+        System.out.println("Expected = " + expected);
+        System.out.println("Actual   = " + actual);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testDivide() {
+        int expected = 5;
+        int actual = calc.divide(10, 2);
+
+        System.out.println("PASS TEST: Division");
+        System.out.println("Expected = " + expected);
+        System.out.println("Actual   = " + actual);
+
+        Assert.assertEquals(actual, expected);
+    }
+}
